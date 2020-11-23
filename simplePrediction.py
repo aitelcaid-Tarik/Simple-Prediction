9c3bb71e40891394b678ece57bbd2343272c7849
admin <aitelcaid.tarik@gmail.com>
2020-11-23 15:00:04 +0100

    first commit

---
+++ simplePrediction.py
@@ -0,0 +1,12 @@
+from sklearn import tree
+
+# [weight g, color ]
+# 2 = red, 3 = yellow , 4 = orange
+features = [[102,2],[102,3],[300,4],[400,4]]
+labels = ['Tfa7a', 'Tfa7a', 'Limouna', 'Limouna']
+
+clasf = tree.DecisionTreeClassifier()
+classif = clasf.fit(features, labels)
+
+print (classif.predict([[160,4]]))
+
